4360d86a4a99609a2433e93265bc7b1c984818de
David Thompson <david.thompson@1accordtech.com>
2022-09-08 22:10:08 -0400

    Added PowerShell script to grab managed devices by serial number and add to groups

---
+++ bulkAddDeviceToGroups.ps1
@@ -0,0 +1,38 @@
+﻿Import-Module AzureAD
+Import-Module MSOnline
+
+# Import CSV file containing Group Names and ObjectIds
+$groups = Import-Csv .\groupIds.csv
+
+function getGroupIdByName($groupName) {
+
+    $result = $groups | Where-Object { $_.groupName -eq $groupName } | Select $_.groupId
+    return $result.groupId
+
+}
+
+$filePath = 'deviceNames.csv'
+
+Import-Csv $filePath | forEach {
+
+    $serial = $_.SerialNumber
+    $filter = "serialNumber eq '$serial'"
+    
+    $intuneDevice = Get-IntuneManagedDevice -Filter $filter
+
+    $intuneDeviceId = "'" + $intuneDevice.id + "'"
+    $deviceName = $_.DeviceName
+    $currentDeviceName = $intuneDevice.deviceName
+
+    $msolDevice = Get-MsolDevice -Name $deviceName
+    
+    $grpId = getGroupIdByName($_.Group)
+    if( $null -ne $msolDevice -and $msolDevice.ObjectId ) {
+        Write-Host "Adding " $deviceName "to" $_.Group
+        Add-AzureADGroupMember -ObjectId $grpId -RefObjectId $msolDevice.ObjectId
+    } else {
+        Write-Host "Unable to locate device" $deviceName
+    }
+
+
+}
